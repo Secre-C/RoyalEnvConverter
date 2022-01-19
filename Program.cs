8350acbb5a7cdd963558be66822b3568b407a1d8
Secre-C <89033534+Secre-C@users.noreply.github.com>
2022-01-18 17:08:46 -0800

    added sln and made conversion more accurate

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using System.Reflection;
+using Amicitia.IO;
 
 namespace Env_Convert_Maybe
 {
@@ -10,6 +11,7 @@ namespace Env_Convert_Maybe
         public static int filePlace = 0;
         public static int fileAmount;
         public static string versionGot;
+        public static string moisty;
         public static int lengthGot;
         public static int versionNum;
         public static bool yn = true;
@@ -34,6 +36,7 @@ namespace Env_Convert_Maybe
             {
                 EnvRead(envFileInput[envPlace]);
                 System.IO.File.Delete("output\\" + envFileInput[envPlace]);
+                deMoist();
                 System.IO.File.Copy("resultfile", "output\\" + envFileInput[envPlace]);
                 System.IO.File.Delete("resultfile");
                 Console.WriteLine("\nSuccessfully Converted\n");
@@ -96,11 +99,23 @@ namespace Env_Convert_Maybe
                     envCoords[1] = 4;
                     envCoords[2] = 0x1C9; // 4 bools at the beginning
                 }
+                else if (lightStep == 1 && (versionNum == 01105090))
+                {
+                    envCoords[0] = 0x1CD;
+                    envCoords[1] = 20;
+                    envCoords[2] = 0x1D3; // bloom amount - glare sensitivity
+                }
                 else if (lightStep == 1)
                 {
                     envCoords[0] = 0x1CD;
                     envCoords[1] = 20;
                     envCoords[2] = 0x1D4; // bloom amount - glare sensitivity
+                }
+                else if (lightStep == 2 && (versionNum == 01105090))
+                {
+                    envCoords[0] = 0x1E1;
+                    envCoords[1] = 16;
+                    envCoords[2] = 0x237; // glare length - glare mode
                 }
                 else if (lightStep == 2)
                 {
@@ -189,6 +204,56 @@ namespace Env_Convert_Maybe
         lengthGot = Convert.ToInt16(lfileLength);
         Console.WriteLine("\nBytes: " + lfileLength);
         binReader.Close();
+        }
+        static void deMoist() //divide f32 Field1F2 by 4
+        {
+            moisty = null;
+            BinaryReader checker = new BinaryReader(new FileStream("resultfile", FileMode.Open));
+            for (int i = 0x1F2; i <= 0x1F5; i++)
+            {
+                checker.BaseStream.Position = i;
+                moisty += checker.ReadByte().ToString("X2");
+            }
+            checker.Close();
+            uint num = uint.Parse(moisty, System.Globalization.NumberStyles.AllowHexSpecifier);
+            byte[] floatVals = BitConverter.GetBytes(num);
+            float f = BitConverter.ToSingle(floatVals, 0);
+            f = f / 4;
+            var bytes = BitConverter.GetBytes(f);
+            var s = BitConverter.ToInt32(bytes, 0);
+            string newfloat = s.ToString("X2");
+            byte[] byteArray = StringToByteArrayFastest(moisty);
+            BinaryWriter div4 = new BinaryWriter(File.OpenWrite("resultfile"));
+            int x = 0x1F2;
+            div4.BaseStream.Position = x;
+            div4.Write(byteArray);
+            div4.Close();
+
+        }
+        public static byte[] StringToByteArrayFastest(string hex)
+        {
+            if (hex.Length % 2 == 1)
+                throw new Exception("The binary key cannot have an odd number of digits");
+
+            byte[] arr = new byte[hex.Length >> 1];
+
+            for (int i = 0; i < hex.Length >> 1; ++i)
+            {
+                arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+            }
+
+            return arr;
+        }
+
+        public static int GetHexVal(char hex)
+        {
+            int val = (int)hex;
+            //For uppercase A-F letters:
+            //return val - (val < 58 ? 48 : 55);
+            //For lowercase a-f letters:
+            //return val - (val < 58 ? 48 : 87);
+            //Or the two combined, but a bit slower:
+            return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
         }
     }
 }
